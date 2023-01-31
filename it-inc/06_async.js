26e7953b97b06bcdbf86a7654ade40a9327bbfc9
rimskayama <rimskayama@outlook.com>
2023-01-31 13:36:55 +0300

    06_async.js

---
+++ it-inc/06_async.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+};
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+const server = http.createServer(async (request, response) => {
+    switch (request.url) {
+        case "/home":
+        {
+            try {
+                const data = await readFile("./resources/pages/home.html");
+                response.write(data);
+                response.end();
+            } catch (err) {
+                response.write("error 500");
+                response.end();
+            }
+        }
+            break;
+        case "/about": {
+            await delay(3000);
+            response.write("about course");
+            response.end();
+            break;
+        }
+        default: {
+            response.write("404 not found");
+            response.end();
+        }
+    }
+});
+
+server.listen(3003);
